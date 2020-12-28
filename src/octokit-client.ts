055202c9003e891ad62e73bdb90d44f748ea4c43
Peter Evans <18365890+peter-evans@users.noreply.github.com>
2020-12-28 11:38:43 +0900

    fix: reference to pulls octokit type

--- src/octokit-client.ts
+++ src/octokit-client.ts
@@ -1,11 +1,14 @@
 import {Octokit as Core} from '@octokit/core'
 import {paginateRest} from '@octokit/plugin-paginate-rest'
-import {restEndpointMethods} from '@octokit/plugin-rest-endpoint-methods'
+import {
+  restEndpointMethods,
+  RestEndpointMethodTypes
+} from '@octokit/plugin-rest-endpoint-methods'
 
-export {RestEndpointMethodTypes} from '@octokit/plugin-rest-endpoint-methods'
 export {OctokitOptions} from '@octokit/core/dist-types/types'
 export const Octokit = Core.plugin(paginateRest, restEndpointMethods)
-export {PullsGetResponseData} from '@octokit/types'
+
+export type PullsGetResponseData = RestEndpointMethodTypes['pulls']['get']['response']['data']
 
 export {graphql} from '@octokit/graphql'
 export {graphql as Graphql} from '@octokit/graphql/dist-types/types'
